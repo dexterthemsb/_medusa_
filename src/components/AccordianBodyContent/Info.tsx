0eb1fe90b0526507d140021b5e43a63f69ff7317
Manmohan Singh <dexterthemsb@gmail.com>
2022-10-05 15:39:09 +0530

    Feat: info section

--- src/components/AccordianBodyContent/Info.tsx
+++ src/components/AccordianBodyContent/Info.tsx
@@ -1,16 +1,41 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Input, Text, Textarea, theme } from "@chakra-ui/react";
 
 const Info = () => {
   return (
-    <Box
-      minH="full"
-      display="flex"
-      flexDir="row"
-      alignItems="center"
-      justifyContent="center"
-      color="gray.500"
-    >
-      Info
+    <Box minH="full">
+      <Input
+        mb="4"
+        py="1"
+        px="2"
+        h="max-content"
+        variant="filled"
+        placeholder="Enter website"
+        defaultValue="https://www.oslash.com"
+      />
+      <Textarea
+        maxLength={200}
+        mb="2"
+        py="1"
+        px="2"
+        h="max-content"
+        resize="none"
+        variant="filled"
+        placeholder="Enter description"
+        defaultValue="OSlash shortcut."
+      />
+      <Text w="full" fontSize="sm">
+        <span
+          style={{
+            display: "inline-block",
+            padding: "4px 8px",
+            margin: "8px 8px 0 0",
+            borderRadius: 9999,
+            backgroundColor: theme.colors.gray[200]
+          }}
+        >
+          #engineering
+        </span>
+      </Text>
     </Box>
   );
 };
